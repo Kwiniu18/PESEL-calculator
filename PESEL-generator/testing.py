370c27333df9a265a426d1b3cfb6aeeaec0ae522
Kwnq <44814268+Kwiniu18@users.noreply.github.com>
2021-12-03 13:54:56 +0100

    Update testing.py

--- PESEL-generator/testing.py
+++ PESEL-generator/testing.py
@@ -1,5 +1,8 @@
 from calculating import Century
 from calculating import Control
+from calculating import Date
+from calculating import Gender
+from calculating import GenderDec
 import pytest
 
 
@@ -16,6 +19,24 @@ def test_control():
 def test_pesel_len():
     pesel = "27926371917"
     assert len(pesel) == 11
+
+def test_pesel_date():
+    result = Date("")
+    assert result.date_test("04260402376") == 1
+
+def test_gender_value():
+    result = Gender("")
+    assert result.gender_value("male") == 1
+    assert result.gender_value("female") == 1
+
+def test_gender_dec():
+    result = GenderDec(4)
+    assert result.gender_dec(4) == 1
+
+
+
+
+
 
 
 
